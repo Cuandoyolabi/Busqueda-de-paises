d7f8c7dbaa23eaebcdc73b4e3bec834c7a66e3fc
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2025-05-19 15:24:53 -0600

    Se inicia el proyecto con git, con tailwind y daysiUI agregado anteriormente

---
+++ app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-by-capital-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './by-capital-page.component.html',
+})
+export class ByCapitalPageComponent {
+
+}
